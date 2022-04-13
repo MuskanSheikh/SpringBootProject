571eeb545b145aea3f7042d4102ebdcc67aa0d81
Muskan <muskansheikh.100.ms@gmail.com>
2022-04-13 14:54:14 +0530

    file Upload & download using filesystem completed

--- src/main/java/com/Spring/SpringBoot/Controller/FileUploadController.java
+++ src/main/java/com/Spring/SpringBoot/Controller/FileUploadController.java
@@ -1,30 +1,93 @@
 package com.Spring.SpringBoot.Controller;
-import com.Spring.SpringBoot.helper.FileUploadHelper;
-import com.Spring.SpringBoot.message.ResponseFile;
+import com.Spring.SpringBoot.Dao.FileData;
+import com.Spring.SpringBoot.message.ResponseMessage;
 import com.Spring.SpringBoot.services.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("files")
 public class FileUploadController {
-    @Autowired
-   // private FileUploadHelper fileUploadHelper;
-    private FileStorageService fileStorageService;
 
-    @PostMapping("/File-upload")
-    ResponseFile uploadFile(@RequestParam("file")MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
-        String url=ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloads")
-                .path(fileName).toUriString();
-        String contentType=file.getContentType();
-        ResponseFile response=new ResponseFile(fileName,contentType, url);
-        return response;
+    private final FileStorageService fileStorageService;
+    @Autowired
+    public FileUploadController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file")MultipartFile file) {
+        try{
+            fileStorageService.save(file);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseMessage("File uploaded successfully:  "+file.getOriginalFilename()));
+        }catch(Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseMessage("Could not upload the file: " + file.getOriginalFilename() + "!"));
+        }
+    }
+
+    @GetMapping
+    public  ResponseEntity<List<FileData>> getListFiles()
+    {
+        List<FileData> fileInfos=fileStorageService.loadAll()
+                .stream()
+                .map(this::pathToFileData)
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(fileInfos);
+    }
+
+    @DeleteMapping
+    public void delete()
+    {
+        fileStorageService.deleteAll();
+    }
+
+    private FileData pathToFileData(Path path) {
+        FileData filedata=new FileData();
+        String filename = path.getFileName().toString();
+        filedata.setFilename(filename);
+        filedata.setUrl(MvcUriComponentsBuilder
+                .fromMethodName(FileUploadController.class,"getFile",filename)
+                .build().toString());
+        try{
+            filedata.setSize(Files.size(path));
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException("Error: "+e.getMessage());
+        }
+        return filedata;
+    }
+
+    @GetMapping("{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename)
+    {
+        Resource file=fileStorageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders
+                .CONTENT_DISPOSITION,"attachment; filename= \""+file.getFilename()+"\"")
+                .body(file);
     }
 
 }
--- src/main/java/com/Spring/SpringBoot/services/FileStorageService.java
+++ src/main/java/com/Spring/SpringBoot/services/FileStorageService.java
@@ -1,14 +1,15 @@
 package com.Spring.SpringBoot.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,7 +23,7 @@ public class FileStorageService {
     @Value("${upload.path}")
     private  String uploadPath;
 
-    @PostConstruct
+    @PostConstruct//this will create an empty directory
     public void init()
     {
         try
@@ -33,7 +34,7 @@ public class FileStorageService {
          throw new RuntimeException("Could not create folder!!!"+e.getMessage());
         }
     }
-
+    //storing the file
     public void save(MultipartFile file)
     {
         try{
@@ -47,7 +48,7 @@ public class FileStorageService {
             throw new RuntimeException("Could not store file"+e.getMessage());
         }
     }
-
+    //upload file
     public Resource load(String filename)
     {
         try{
@@ -60,11 +61,27 @@ public class FileStorageService {
         }
     }
 
+    //download file
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = Paths.get(uploadPath).resolve(fileName).normalize();
+            Resource resource = (Resource) new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("File not found " + fileName, ex);
+        }
+    }
+
+    //delete all files
     public  void deleteAll()
     {
         FileSystemUtils.deleteRecursively(Paths.get(uploadPath).toFile());
     }
-
+    //list all files
     public List<Path> loadAll()
     {
         try{
