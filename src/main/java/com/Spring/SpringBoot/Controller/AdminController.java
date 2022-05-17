29be03be3d3f993c5222d0343c1dfd54fb213a6c
Muskan <muskansheikh.100.ms@gmail.com>
2022-05-17 12:30:12 +0530

    create api of addCategory and Listcategory

--- src/main/java/com/Spring/SpringBoot/Controller/AdminController.java
+++ src/main/java/com/Spring/SpringBoot/Controller/AdminController.java
@@ -1,11 +1,20 @@
 package com.Spring.SpringBoot.Controller;
 
+import com.Spring.SpringBoot.Dao.CategoryDao;
+import com.Spring.SpringBoot.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
 
     @GetMapping(value="/adminPanel")
     public String adminDashboard(Model model)
@@ -16,6 +25,7 @@ public class AdminController {
     @GetMapping(value="/categories")
     public String getcategories(Model model)
     {
+        model.addAttribute("catagories", categoryDao.findAll());
         model.addAttribute("title","List Categories");
         return "ViewCategories";
     }
@@ -23,7 +33,17 @@ public class AdminController {
     @GetMapping(value ="/addCategory")
     public String addCategories(Model model)
     {
+        model.addAttribute("category", new Category());
         model.addAttribute("title","Add Categories");
         return "AddCategory";
     }
+
+    @PostMapping(value="/addcategory")
+    public String getAllCat(@ModelAttribute("category") Category category)
+    {
+        categoryDao.save(category);
+        return "ViewCategories";
+    }
+
+
 }
